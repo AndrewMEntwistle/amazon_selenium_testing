8fe5f77abf2d5a5ee13d0fb13580e32ed3b594ce
andrew.entwistle <AndrewMEntwistle@gmail.com>
2019-09-30 23:26:36 +0100

    refactored step defs to put classes on the context variable to prevent more class instances than necessary

--- features/steps/product_page.py
+++ features/steps/product_page.py
@@ -1,5 +1,4 @@
 from behave import *
-
 from pages.homepage import Homepage
 from pages.product_page import ProductPage
 from pages.search_results_page import SearchResultsPage
@@ -7,14 +6,13 @@ from pages.search_results_page import SearchResultsPage
 
 @given('I navigate to the homepage')
 def step_impl(context):
-    homepage = Homepage(context.browser)
-    homepage.navigate_to()
+    context.homepage = Homepage(context.browser)
+    context.homepage.navigate_to()
 
 
 @given('I search for {search_term}')
 def step_impl(context, search_term):
-    homepage = Homepage(context.browser)
-    homepage.search(search_term)
+    context.homepage.search(search_term)
 
 
 @when('I select {product}')
@@ -25,18 +23,16 @@ def step_impl(context, product):
 
 @then('The price of the "Audio CD" is {price}')
 def step_impl(context, price):
-    product_page = ProductPage(context.browser)
-    product_page.assert_price_of_audio_cd(price)
+    context.product_page = ProductPage(context.browser)
+    context.product_page.assert_price_of_audio_cd(price)
 
 
 @then('The star rating is {rating}')
 def step_impl(context, rating):
-    product_page = ProductPage(context.browser)
-    product_page.assert_star_rating(rating)
+    context.product_page.assert_star_rating(rating)
 
 
 @then(
     'The first item in the "customers who bought this item also bought" section is {first_item_customers_also_bought}')
 def step_impl(context, first_item_customers_also_bought):
-    product_page = ProductPage(context.browser)
-    product_page.assert_first_item_customers_also_bought(first_item_customers_also_bought)
+    context.product_page.assert_first_item_customers_also_bought(first_item_customers_also_bought)
