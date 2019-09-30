b8d24ee2770a8bc12da8c5eafa2baff070092c03
andrew.entwistle <AndrewMEntwistle@gmail.com>
2019-09-30 20:05:15 +0100

    Setup environment variable for the Chromedriver

--- features/environment.py
+++ features/environment.py
@@ -1,9 +1,11 @@
 from behave import fixture, use_fixture
 from selenium import webdriver
+import os
+
 
 @fixture
 def selenium_browser_chrome(context):
-    context.browser = webdriver.Chrome("C:/Users/1083517/Downloads/chromedriver_win32 (1)/ChromeDriver.exe") #make an env variable for chromedriver
+    context.browser = webdriver.Chrome(os.environ.get('WEBDRIVER'))
     yield context.browser
     context.browser.quit()
 
